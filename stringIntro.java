f72a19794d68a2950641d9b4c921f07fe8272456
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2016-11-28 22:13:08 -0600

    Update stringIntro.java

--- stringIntro.java
+++ stringIntro.java
@@ -4,23 +4,12 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
-        int sum = A.length()+B.length();
-        System.out.println(sum);
-        if(A.compareTo(B) > 0){
-            System.out.println("Yes");
-        }
-        else if(A.compareTo(B) < 0){
-            System.out.println("No");
-        }
-        else
-            System.out.println("Equivalent");
-        char a = Character.toUpperCase(A.charAt(0));
-        char b = Character.toUpperCase(B.charAt(0));
-        System.out.println(A.replace(A.charAt(0), a)+" "+B.replace(B.charAt(0), b));
+        System.out.println(A.length()+B.length());
+        System.out.println(A.compareTo(B)>0?"Yes":"No");
+        System.out.println( A.substring(0, 1).toUpperCase()+A.substring(1)+" "+B.substring(0, 1).toUpperCase()+B.substring(1));
         
     }
 }
